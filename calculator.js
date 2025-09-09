985fb859836a0d51a2f572dcb18794220a430b40
kmrober3 <kmroberts@unc.edu>
2025-09-09 18:25:10 -0400

    Initialized JS, HTML, and CSS files

---
+++ calculator.js
@@ -0,0 +1,32 @@
+class Calculator {
+    
+    //Returns result
+    answer(input) {
+
+    } 
+
+    //Utilized to determine if individual character is letter or digit
+    isletterOrDigit(x) {
+
+    } 
+
+    //Utilized to compute addition 
+    addition(a, b) {
+
+    } 
+
+    //Utilized to compute subtraction
+    subtraction(a, b) {
+
+    } 
+
+    //Utilized to compute multiplication 
+    multiplication(a, b) {
+
+    } 
+
+    //Utilized to compute division
+    division(a, b) {
+
+    }
+}
