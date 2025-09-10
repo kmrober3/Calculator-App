dbdcb5d7bc37507b6a20a0af77582b47ca3ec3db
kmrober3 <kmroberts@unc.edu>
2025-09-10 15:24:59 -0400

    Simplified deserialization method

--- calculator.js
+++ calculator.js
@@ -22,6 +22,9 @@ class Calculator {
             } else if (this.map.has(input[i])){
                 copy[x] = input[i]; 
                 x++;
+            } else if (this.isDecimal(input[i])) {
+                copy[x] = input[i];
+                x++;
             } else {
                 continue;
             }
@@ -31,64 +34,30 @@ class Calculator {
         this.deserializeInput(serializedCopy);
     }   
 
-    //Performs operations on input
-    deserializeInput(copy) {  
-        let i = 0; 
-        let max = copy.length;   
-        let flag = false;  
-        let result = 0;
-        let a = 0;
-        let b = 0;  
-        while (i < max) {
-            if (this.isletterOrDigit(copy[i]) && flag == false) { 
-                a = Number.parseInt(copy[i]);
-                flag = true;
-                i++;
-                continue;
-            } else if (!this.isletterOrDigit(copy[i]) && this.map.has(copy[i])) {   
-                i++;
-                b = Number.parseInt(copy[i]);
-                if (copy[i] == "+") { 
-                    result += this.addition(a, b);  
-                } else if (copy[i] == "-") {
-                    result += this.subtraction(a, b);
-                } else if (copy[i] == "*") { 
-                    result += this.multiplication(a,b);
-                } else if (copy[i] == "/") { 
-                    result += this.division(a, b);
-                } 
-                i++;
-            }
-        }  
-        console.log(result);
-        return result;
+    //Processes input
+    deserializeInput(copy) {   
+        try { 
+            let result = Function('"use strict"; return (' + copy + ')')();
+            console.log("Result: " + result);
+            return result;
+        } catch(err) { 
+            console.log("Invalid input: " + copy);
+            return null;
+        }
     }
 
     //Utilized to determine if individual character is letter or digit
     isletterOrDigit(x) {  
         return /^[a-zA-Z0-9]$/.test(x);
-    } 
+    }  
 
-    //Utilized to compute addition 
-    addition(a, b) {
-        return a + b;
-    } 
-
-    //Utilized to compute subtraction
-    subtraction(a, b) { 
-        return a-b;
-    }
-
-    //Utilized to compute multiplication 
-    multiplication(a, b) { 
-        return a * b;
-    } 
-
-    //Utilized to compute division
-    division(a, b) { 
-        return a / b;
+    isDecimal(x) {
+        return /^[.]$/.test(x);
     }
 }   
+
+
+//Testing Code
 
 let instance = new Calculator();  
 
